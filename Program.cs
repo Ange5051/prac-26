59e7ec8ec2e68e7423bb73f3474c808846816276
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2025-02-28 12:50:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car car = new car();
+            car.Model = "Мерсидес";
+            car.Year = 2025;
+
+            Console.WriteLine("Введте марку бренд");
+            string brand1 = Console.ReadLine();
+            int years1 = int.Parse(Console.ReadLine());
+
+            car car1 = new car(brand1, years1);
+            car car2 = new car("АМГ", 2025);
+            car car12 = new car("Мерсидес", 2025);
+            car car13 = new car("БМВ", 2025);
+            car car14 = new car("Ролс Ройс", 2025);
+            Console.WriteLine($"{car12.Model}{car12.Year}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class car
+    {
+        string model;
+        int year;
+        public static int totalCars = 0;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public car() { }
+        public car(string model, int year)
+        {
+            this.Model = model;
+            this.Year = year;
+            totalCars++;
+        }
+    }
+}
